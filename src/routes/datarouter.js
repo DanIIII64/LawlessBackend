3314b394e0e49fd985f847a63d661ee22c0d8e1d
DanIIII64 <egridaniel198@gmail.com>
2026-04-12 14:39:54 +0200

    the crash is fixed, downloading profiles is stuck
    im happy

--- src/routes/datarouter.js
+++ src/routes/datarouter.js
@@ -2,12 +2,10 @@ const express = require('express');
 const app = express();
 const sdk = require("../responses/sdkv1.json");
 
-app.post('/datarouter/api/v1/public/data', (req, res) => {
-    res.status(200).send({
-    status: "OK",
-    code: 200
-});
-console.log(`Post /datarouter/api/v1/public/data called`);
+app.post("/datarouter/api/v1/public/data", async (req, res) => {
+    res.status(204);
+    res.end();
+    console.log(`Post /datarouter/api/v1/public/data called`);
 });
 
 app.post('/datarouter/api/v1/public/data/clients', (req, res) => {
