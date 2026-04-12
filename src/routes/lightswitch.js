8098e3f0bc451f49af03ba97627005fdec2c6301
DanIIII64 <egridaniel198@gmail.com>
2026-04-12 14:26:56 +0200

    Update lightswitch.js
    maybe fix?

--- src/routes/lightswitch.js
+++ src/routes/lightswitch.js
@@ -25,20 +25,15 @@ app.get('/lightswitch/api/service/bulk/status', (req, res) => {
     res.status(200).send({
     "serviceInstanceId": "fortnite",
     "status": "UP",
-    "message": "fortnite is up.",
+    "message": "Fortnite is online",
     "maintenanceUri": null,
-    "overrideCatalogIds": [
-        "a7f138b2e51945ffbfdacc1af0541053"
-    ],
-    "allowedActions": [
-        "PLAY",
-        "DOWNLOAD"
-    ],
+    "overrideCatalogIds": ["a7f138b2e51945ffbfdacc1af0541053"],
+    "allowedActions": [],
     "banned": false,
     "launcherInfoDTO": {
-        "appName": "Fortnite",
-        "catalogItemId": "4fe75bbc5a674f4f9b356b5c90567da5",
-        "namespace": "fn"
+      "appName": "Fortnite",
+      "catalogItemId": "4fe75bbc5a674f4f9b356b5c90567da5",
+      "namespace": "fn"
     }
 });
 console.log(`Get /lightswitch/api/service/bulk/status called`);
