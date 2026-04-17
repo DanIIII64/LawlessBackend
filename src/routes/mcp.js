1b62b4bb76072d608dc085848c997869991db0df
DanIIII64 <egridaniel198@gmail.com>
2026-04-17 15:08:31 +0200

    sum mcp

--- src/routes/mcp.js
+++ src/routes/mcp.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const functions = require("./functions.js");
+const fs = require("fs");
+const path = require("path");
 
 app.post('/fortnite/api/game/v2/profile/:accountId/client/:operation', (req, res) => {
     const { accountId, operation } = req.params;
@@ -9,6 +12,46 @@ app.post('/fortnite/api/game/v2/profile/:accountId/client/:operation', (req, res
 });
 console.log(`Post /fortnite/api/game/v2/profile/${accountId}/client/${operation} called`);
 });
+
+app.use((req, res, next) => {
+    if (!req.query.profileId && req.originalUrl.toLowerCase().startsWith("/fortnite/api/game/v2/profile/")) {
+        return res.status(404).json({
+            error: "Profile not defined."
+        });
+    }
+
+    fs.readdirSync("./profiles").forEach((file) => {
+        if (file.endsWith(".json")) {
+            const memory = functions.GetVersionInfo(req);
+
+            const profile = require(`../../profiles/${file}`);
+            if (!profile.rvn) profile.rvn = 0;
+            if (!profile.items) profile.items = {}
+            if (!profile.stats) profile.stats = {}
+            if (!profile.stats.attributes) profile.stats.attributes = {}
+            if (!profile.commandRevision) profile.commandRevision = 0;
+
+            if (file == "athena.json") {
+                var SeasonData = JSON.parse(JSON.stringify(require("../responses/Athena/SeasonData.json")));
+                profile.stats.attributes.season_num = memory.season;
+
+                if (SeasonData[`Season${memory.season}`]) {
+                    SeasonData = SeasonData[`Season${memory.season}`];
+
+                    profile.stats.attributes.book_purchased = SeasonData.battlePassPurchased;
+                    profile.stats.attributes.book_level = SeasonData.battlePassTier;
+                    profile.stats.attributes.season_match_boost = SeasonData.battlePassXPBoost;
+                    profile.stats.attributes.season_friend_match_boost = SeasonData.battlePassXPFriendBoost;
+                }
+
+                fs.writeFileSync("./profiles/athena.json", JSON.stringify(profile, null, 2));
+            }
+        }
+    })
+
+    return next();
+});
+
 
 app.get("/waitingroom/api/waitingroom", (req, res) => {
     res.status(204);
